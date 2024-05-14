650cc2a679c45fd01fdeb8ad9fce6bc2d05fe24f
ABHIRAMK1998 <abhiramsunil444@gmail.com>
2024-05-14 10:28:42 +0530

    initial coomit

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.userregistration, name='register'),
+    path('', views.loginPage, name='login'),
+    path('admin_view/', views.admin_view, name='admin_view'),
+    path('user_view/', views.user_view, name='user_view'),
+    path('logout/', views.logout_view, name='logout')
+]
